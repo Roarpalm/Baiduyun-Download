c55f8e49e01c5e33767074a6ed1ce52b615f4ef3
Roarpalm <307687673@qq.com>
2020-07-18 16:41:38 +0800

    优化代码

--- C#/WTF/百度云链接解析.cs
+++ C#/WTF/百度云链接解析.cs
@@ -17,51 +17,63 @@ namespace BaiduyunLink
             Console.WriteLine("请完整复制粘贴百度云分享链接然后回车\n");
             Console.WriteLine("请确保链接和提取码之间有空格无回车：");
             string text = Convert.ToString(Console.ReadLine());
-            string link_reg = @"(?<=https://pan.baidu.com/s/)\S+";
-            MatchCollection linkMatch = Regex.Matches(text, link_reg);
-            foreach (Match m in linkMatch)
+            Regex link_reg = new Regex(@"(?<=https://pan.baidu.com/s/)\S+");
+            Match link_match = link_reg.Match(text);
+            if(link_match.Success)
             {
-                link = m.Value;
+                link = link_match.Value;
             }
-            string code_reg = @"(?<=码: )\w+";
-            MatchCollection codeMatch = Regex.Matches(text, code_reg);
-            foreach (Match a in codeMatch)
-            {
-                code = a.Value;
-            }
-            code_reg = @"(?<=码:)\w+";
-            codeMatch = Regex.Matches(text, code_reg);
-            foreach (Match b in codeMatch)
-            {
-                code = b.Value;
-            }
-            code_reg = @"(?<=码： )\w+";
-            codeMatch = Regex.Matches(text, code_reg);
-            foreach (Match c in codeMatch)
-            {
-                code = c.Value;
-            }
-            code_reg = @"(?<=码：)\w+";
-            codeMatch = Regex.Matches(text, code_reg);
-            foreach (Match d in codeMatch)
-            {
-                code = d.Value;
-            }
-            code_reg = @"(?<=码)\w+";
-            codeMatch = Regex.Matches(text, code_reg);
-            foreach (Match e in codeMatch)
-            {
-                code = e.Value;
-            }
-            if(link == "")
+            else
             {
                 Console.WriteLine("未匹配到链接\n");
                 goto final;
             }
-            if(code == "")
+            Regex code_reg = new Regex(@"(?<=码: )\w+");
+            Match code_match = code_reg.Match(text);
+            if(code_match.Success)
             {
-                Console.WriteLine("未匹配到提取码\n");
-                goto final;
+                code = code_match.Value;
+            }
+            else
+            {
+                code_reg = new Regex(@"(?<=码:)\w+");
+                code_match = code_reg.Match(text);
+                if (code_match.Success)
+                {
+                    code = code_match.Value;
+                }
+                else
+                {
+                    code_reg = new Regex(@"(?<=码： )\w+");
+                    code_match = code_reg.Match(text);
+                    if (code_match.Success)
+                    {
+                        code = code_match.Value;
+                    }
+                    else
+                    {
+                        code_reg = new Regex(@"(?<=码：)\w+");
+                        code_match = code_reg.Match(text);
+                        if (code_match.Success)
+                        {
+                            code = code_match.Value;
+                        }
+                        else
+                        {
+                            code_reg = new Regex(@"(?<=码)\w+");
+                            code_match = code_reg.Match(text);
+                            if (code_match.Success)
+                            {
+                                code = code_match.Value;
+                            }
+                            else
+                            {
+                                Console.WriteLine("未匹配到提取码\n");
+                                goto final;
+                            }
+                        }
+                    }
+                }
             }
             string url = $"http://pan.naifei.cc/?share={link}%20&pwd={code}";
             Console.WriteLine("\n官网：");
@@ -76,77 +88,62 @@ namespace BaiduyunLink
         public static void Spider(string url)
         {
             string href = null;
+            string name = null;
+            string size = null;
             HtmlNode node = null;
+            HtmlNode name_node = null;
+            HtmlNode size_node = null;
             HtmlDocument doc = null;
-            try
-            {
-                HtmlWeb web = new HtmlWeb();
-                doc = web.Load(url);
-                node = doc.DocumentNode.SelectSingleNode("/html/body/table/tbody/tr/td[3]/a/@href");
-                href = node.OuterHtml;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine($"Error：未找到下载链接\n");
-                return;
-            }
             string reg = @"<a[^>]*href=([""'])?(?<href>[^'""]+)\1[^>]*>";
-            MatchCollection matches = Regex.Matches(href, reg, RegexOptions.IgnoreCase);
-            int n = 1;
-            foreach (Match item in matches)
+            HtmlWeb web = new HtmlWeb();
+            doc = web.Load(url);
+            for (int i = 1; i < 99; i++)
             {
-                var href2 = item.Groups["href"].Value;
-                Console.WriteLine($"第{n}个下载链接：");
-                Console.WriteLine(href2);
-                try
+                try                    
                 {
-                    // 使用指定浏览器
-                    //Process.Start(@"D:\Chrome\Application\chrome.exe", TargetUrl);
-
-                    // 使用默认浏览器，自动打开下载链接
-                    Console.WriteLine("2秒后自动打开浏览器");
-                    Thread.Sleep(2000);
-                    Process.Start(href2);
-                }
-                catch (Exception other)
-                {
-                    Console.WriteLine("Error" + other.Message);
-                }
-            }
-            for (int i = 2; i < 99; i++)
-            {
-                try
-                {
-                    n++;
+                    //href
                     node = doc.DocumentNode.SelectSingleNode($"/html/body/table/tbody/tr[{i}]/td[3]/a/@href");
-                    if (node != null)
+                    href = node.OuterHtml;
+                    //文件大小
+                    size_node = doc.DocumentNode.SelectSingleNode($"/html/body/table/tbody/tr[{i}]/td[2]");
+                    size = size_node.InnerText;
+                    //文件为0直接跳过
+                    if (size == "0M")
                     {
-                        href = node.OuterHtml;
+                        continue;
                     }
-                    else break;
-                    matches = Regex.Matches(href, reg, RegexOptions.IgnoreCase);
-                    foreach (Match item in matches)
-                    {
-                        var href2 = item.Groups["href"].Value;
-                        Console.WriteLine($"第{n}个下载链接：");
-                        Console.WriteLine(href2);
-                        try
-                        {
-                            //Process.Start(@"D:\Chrome\Application\chrome.exe", TargetUrl);
-                            Console.WriteLine("2秒后自动打开浏览器");
-                            Thread.Sleep(2000);
-                            Process.Start(href2);
-                        }
-                        catch (Exception other)
-                        {
-                            Console.WriteLine(other.Message);
-                        }
-                    }
+                    //文件名
+                    name_node = doc.DocumentNode.SelectSingleNode($"/html/body/table/tbody/tr[{i}]/td[1]");
+                    name = name_node.InnerText;
+
+                    Console.WriteLine(name + " 大小：" + size);
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
-                    Console.WriteLine($"Error: {e.Message}");
+                    if(i == 1)
+                    {
+                        Console.WriteLine("未找到下载链接");
+                    }
                     break;
+                }                    
+                MatchCollection matches = Regex.Matches(href, reg, RegexOptions.IgnoreCase);
+                foreach (Match item in matches)
+                {
+                    var href2 = item.Groups["href"].Value;
+                    Console.WriteLine($"第{i}个下载链接：");
+                    Console.WriteLine(href2);
+                    try
+                    {
+                        // 使用指定浏览器
+                        //Process.Start(@"D:\Chrome\Application\chrome.exe", TargetUrl);
+
+                        // 使用默认浏览器，自动打开下载链接
+                        Process.Start(href2);
+                    }
+                    catch (Exception other)
+                    {
+                        Console.WriteLine("Error: " + other.Message);
+                    }
                 }
             }
         }
